5d15b4a803248bbce8d773ec7fef9f59405a3cdf
Troels Abrahamsen <troels@blck.dk>
2014-05-20 11:21:19 +0200

    New fields added to options panel

--- inc/register-acf-fields.php
+++ inc/register-acf-fields.php
@@ -1,8 +1,8 @@
 <?php if(function_exists("register_field_group"))
 {
 	register_field_group(array (
-		'id' => 'acf_woocommerce-subshop-options',
-		'title' => 'Woocommerce subshop options',
+		'id' => 'acf_wssoptions',
+		'title' => 'wss:options',
 		'fields' => array (
 			array (
 				'key' => 'field_5371130fd8d87',
@@ -51,6 +51,45 @@
 		),
 		'options' => array (
 			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+	register_field_group(array (
+		'id' => 'acf_wsspages',
+		'title' => 'wss:pages',
+		'fields' => array (
+			array (
+				'key' => 'field_5374664958f90',
+				'label' => 'Assign to subshop',
+				'name' => 'wss_in_subshop',
+				'type' => 'post_object',
+				'instructions' => 'Assigning a page to a subshop will also affect all child pages',
+				'post_type' => array (
+					0 => 'woo_subshop',
+				),
+				'taxonomy' => array (
+					0 => 'all',
+				),
+				'allow_null' => 1,
+				'multiple' => 0,
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'page',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'side',
 			'layout' => 'no_box',
 			'hide_on_screen' => array (
 			),
