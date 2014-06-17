f2d99f07838a6ab248ca9681ffb2d25cc486b8dd
Troels Abrahamsen <troels@blck.dk>
2014-06-17 15:01:02 +0200

    New location rule for ACF

--- inc/acf/wss_acf.php
+++ inc/acf/wss_acf.php
@@ -15,14 +15,17 @@ class wss_acf extends wss_init {
         add_filter('acf/location/rule_types', array('wss_acf', 'location_rules'));
         add_filter('acf/location/rule_values/assigned_shop_id', array('wss_acf', 'location_rule_assigned_shop_id'));
         add_filter('acf/location/rule_values/assigned_shop_name', array('wss_acf', 'location_rule_assigned_shop_name'));
+        add_filter('acf/location/rule_values/user_has_shop', array('wss_acf', 'location_rule_user_has_shop'));
         add_filter('acf/location/rule_match/assigned_shop_name', array('wss_acf', 'match_rule_assigned_shop_name'), 10, 3);
+        add_filter('acf/location/rule_match/user_has_shop', array('wss_acf', 'match_rule_user_has_shop'), 10, 3);
 
     }
     
     function location_rules($choices)
     {
-        $choices['WSS']['assigned_shop_id'] = 'Assigned shop (by ID)';
-        $choices['WSS']['assigned_shop_name'] = 'Assigned shop (by name)';
+        $choices['WSS']['assigned_shop_id']     = 'Assigned shop (by ID)';
+        $choices['WSS']['assigned_shop_name']   = 'Assigned shop (by name)';
+        $choices['WSS']['user_has_shop']        = 'User is in shop';
      
         return $choices;
     }
@@ -33,6 +36,18 @@ class wss_acf extends wss_init {
         if($shops){
             foreach($shops as $shop){
                 $choices[ $shop->ID ] = $shop->post_title.' ('.$shop->ID.')';
+            }
+        }
+     
+        return $choices;
+    }
+
+    function location_rule_user_has_shop($choices)
+    {
+        $shops = wss::get(array('posts_per_page' => -1));
+        if($shops){
+            foreach($shops as $shop){
+                $choices[ $shop->ID ] = $shop->post_title;
             }
         }
      
@@ -52,7 +67,7 @@ class wss_acf extends wss_init {
 
 
     function match_rule_assigned_shop_name($match, $rule, $options){
-        if($options['post_type'] == 'product'){
+        if($options['post_type'] === 'product'){
 
             $shop = $rule['value'];
             if($shop = new wss_subshop($shop)){
@@ -60,8 +75,7 @@ class wss_acf extends wss_init {
                 ob_start();
                 require 'location_rules/acf_location_rule_js.php';
                 $js = ob_get_clean();
-                global $assets;
-                self::inlinewp()->enqueue_js($js);
+                self::inlinewp('wss')->enqueue_js($js);
 
                 if($rule['operator'] == "=="){
                     $match = $shop->has_product($options['post_id']);
@@ -75,9 +89,34 @@ class wss_acf extends wss_init {
             }
 
         }
-     
+
         return $match;
     }
+
+
+    function match_rule_user_has_shop($match, $rule, $options){
+        if($options['post_type'] == 'product'){
+
+            $shop = $rule['value'];
+            if($shop = new wss_subshop($shop)){
+                
+                $userid = get_current_user_id();
+
+                if($rule['operator'] == "=="){
+                    $match = $shop->has_user($userid);
+                }
+                elseif($rule['operator'] == "!="){
+                    if(!$match = $shop->has_user($userid))
+                        $match = true;
+                }
+
+            }
+
+        }
+
+        return $match;
+    }
+
 }
 
 
